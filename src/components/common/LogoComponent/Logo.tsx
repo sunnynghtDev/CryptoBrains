d8da613581608405273de85d519f18ad77c0347b
himorryyyDev <himorryyydev@gmail.com>
2025-07-30 15:48:11 +0300

    logo component

--- src/components/common/LogoComponent/Logo.tsx
+++ src/components/common/LogoComponent/Logo.tsx
@@ -1,8 +1,9 @@
 import logo from '../../../assets/images/logo.svg'
+import styles from './Logo.module.css'
 
 const Logo = () => {
   return (
-    <a href='/'>
+    <a href='/' className={styles.logo__container}>
       <img src={logo} alt="CryptoBrains Home Page" />
     </a>
   )
